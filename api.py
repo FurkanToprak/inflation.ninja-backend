dbc77a7ba4870c5bafd9b3286a4c95e7b04d84d9
Furkan Toprak <furkancemaltoprak@gmail.com>
2022-06-12 20:17:34 -0500

    casting + data min

--- api.py
+++ api.py
@@ -7,11 +7,33 @@ def getWatchList(): # TODO: get 1000 top stocks by volume
     return ["IBM", "AAPL"]
 
 def fetchStock(ticker):
+    """
+    Returns {
+        "YYYY-MM-DD": {
+        "1. open": X.Y,
+        "2. high": X.Y,
+        "3. low": X.Y,
+        "4. close": X.Y,
+        "5. volume": X.Y
+    },
+    }
+    """
     url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker}&apikey={ALPHA_VANTAGE_SECRET}'
     fetchedData = requests.get(url)
     fetchedJson = fetchedData.json()
     timeSeries = fetchedJson['Time Series (Daily)']
-    return timeSeries
+    stock = dict()
+    for day in timeSeries.keys():
+        datum = timeSeries[day]
+        highValue = float(datum["2. high"])
+        lowValue = float(datum["3. low"])
+        volumeValue = int(datum["5. volume"])
+        stock[day] = {
+            'high': highValue,
+            'low': lowValue,
+            'volume': volumeValue,
+        }
+    return stock
 
 def fetchCPI() -> dict:
     """
